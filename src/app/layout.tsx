107cbb434e42287a9a32f6c13ee5f4a6ef8d8daf
OtabekTohirovich <tohirovo630@gmail.com>
2023-08-10 17:00:04 +0500

    Learning next.js-13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Fira_Code({ subsets: ['latin'], weight: "400" })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="uz">
+      <body className={inter.className}>
+        <p>Navbar</p>
+        {children}
+        <p>Footer</p>
+        </body>
     </html>
   )
 }
